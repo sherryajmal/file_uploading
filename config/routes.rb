1dce0c9b4533887f2314ecf3713a2d5f632e0ed0
Hafiz Ahmed <syed.ahmed.shah.434@gmail.com>
2019-04-07 12:31:31 +0500

    Device integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-	root 'dashboards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+	root 'dashboards#index'
 end
